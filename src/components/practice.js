6ba2a5295395087b120afd392f15bf45583e507b
Tony Emenyonu <tonyemenyonu1@gmail.com>
2020-08-10 19:50:43 -0400

    campsite info assignment

---
+++ src/components/practice.js
@@ -0,0 +1,22 @@
+
+
+class CampInfo extends Component {
+    render() { 
+        if(this.props.campsite){
+            return(
+                <div className='row'>
+
+                </div>
+            )
+        }else{
+           return(
+               <div>
+                   
+               </div>
+           ) 
+        }
+        return (  );
+    }
+}
+ 
+export default CampInfo;
