11c0f1c1bb7a927a98cd07efb24b3ab9e573f17c
Tony Emenyonu <tonyemenyonu1@gmail.com>
2020-08-27 22:55:00 -0400

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -22,7 +22,7 @@ const minLength = len => val => val && (val.length >= len);
         );
     }
      
-    function RenderComments({comments, addComment, campsiteId}) {
+    function RenderComments({comments, postComment, campsiteId}) {
         if (comments) {
             return (
                 <div className='col-md-5 m-1'>
@@ -37,7 +37,7 @@ const minLength = len => val => val && (val.length >= len);
                             </div>
                         );
                     })}
-                    <CommentForm campsiteId={campsiteId} addComment={addComment} />       
+                    <CommentForm campsiteId={campsiteId} postComment={postComment} />       
                 </div>
             );
         }
@@ -62,7 +62,7 @@ const minLength = len => val => val && (val.length >= len);
 
         handleSubmit(values) {
             this.toggleModal();
-            this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+            this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
         }
         
         render() { 
@@ -178,7 +178,7 @@ const minLength = len => val => val && (val.length >= len);
                         <RenderCampsite campsite={props.campsite} />
                         <RenderComments 
                         comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         campsiteId={props.campsite.id}
                     />
                     </div>
